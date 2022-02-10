b7f89c5a2fb10caae4fdc9d42fe8988e9f33694d
unknown <neto.estanislau@gmail.com>
2022-02-10 18:31:28 -0300

    Aula sobre Sintaxe Basica JavaScript

---
+++ aula 2/script.js
@@ -0,0 +1,14 @@
+//Tipos Primtivos
+
+//Boolean
+var vOuF = false;
+console.console.log(vOuF);
+
+//Number
+var numeroQualquer = 1;
+console.log(numeroQualquer);
+
+//String
+
+var nome = "Neto";
+console.console.log(nome);
