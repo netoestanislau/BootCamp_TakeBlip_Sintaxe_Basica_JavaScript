c4e3d44b1cf356ba709a2ef9c771977f6e4b22a9
unknown <neto.estanislau@gmail.com>
2022-02-11 18:02:12 -0300

    Aula 4 estruturas condicionais

---
+++ aula 4/script.js
@@ -0,0 +1,11 @@
+var jogador1 = 0;
+var jogador2 = 0;
+var placar;
+
+if(jogador1 > 0){
+    console.timeLog('Jogador 1 marcou ponto');
+} else if (jogador2 > 0){
+    console.log('Jogador 2 marcou ponto');
+} else{
+    console.log('Ninguém marcou');
+}
