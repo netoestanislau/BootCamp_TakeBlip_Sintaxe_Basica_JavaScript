c4e1a121d99aa1db03a222e8041875afaffced2c
unknown <neto.estanislau@gmail.com>
2022-02-11 18:19:38 -0300

    Aula 5 Funções

---
+++ aula 5/script.js
@@ -0,0 +1,11 @@
+/*function funcao(){
+    console.log('Aula sobre Função');
+}
+
+funcao();*/
+
+function mensagem(primeiro, segundo){
+    console.log(primeiro, segundo);
+}
+
+mensagem('Função', 'Por parametro');
